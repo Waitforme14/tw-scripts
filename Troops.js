b7acd6eee7d37cea006246cc6e03946d925eb400
Waitforme14 <joaoamado2001@gmail.com>
2026-04-09 00:02:35 +0100

    Troops.js

--- Troops.js
+++ Troops.js
@@ -259,4 +259,189 @@ function fnCalculateTroopCount() {
     var summary = {
         unitTotal: { tally: 0, population: 0 },
         defense: { tally: 0, count: 0, population: 0, coords: [] },
-        offense: { tally: 0, count: 0, population: 0,
+        offense: { tally: 0, count: 0, population: 0, coords: [] },
+    };
+
+    $(unitConfig).children().each(function (i, e) { summary[e.nodeName] = { tally: 0, count: 0, population: 0, coords: [] }; });
+
+    for (item in outputSummary) {
+        if (outputSummary.hasOwnProperty(item)) summary[item] = { tally: 0, count: 0, population: 0, coords: [] };
+    }
+
+    var villageTroops = fnGetTroopCount();
+    for (ii = 0; ii < villageTroops.length; ii++) {
+        village = villageTroops[ii];
+        total = { defense: { tally: 0, count: 0, population: 0, coords: [] }, offense: { tally: 0, count: 0, population: 0, coords: [] } };
+
+        $(unitConfig).children().each(function (i, e) { total[e.nodeName] = { tally: 0, count: 0, population: 0, coords: [] }; });
+
+        index = 0;
+        $(unitConfig).children().each(function (i, e) {
+            var unit = e.nodeName;
+            total[unit].count += village.troops[index];
+            total[unit].population += village.troops[index] * parseInt($(e).find('pop').text(), 10);
+
+            if (new RegExp('^(' + defense.join('|') + ')$').test(unit)) {
+                total.defense.count += total[unit].count;
+                total.defense.population += total[unit].population;
+            }
+            if (new RegExp('^(' + offense.join('|') + ')$').test(unit)) {
+                total.offense.count += total[unit].count;
+                total.offense.population += total[unit].population;
+            }
+
+            summary[unit].count += total[unit].count;
+            summary[unit].population += total[unit].population;
+            summary.unitTotal.tally += total[unit].count;
+            summary.unitTotal.population += total[unit].population;
+            index++;
+        });
+
+        summary.defense.count += total.defense.count;
+        summary.defense.population += total.defense.population;
+        summary.offense.count += total.offense.count;
+        summary.offense.population += total.offense.population;
+
+        for (item in outputSummary) {
+            if (outputSummary.hasOwnProperty(item)) {
+                isValid = true;
+                for (jj = 0; jj < outputSummary[item].criteria.length; jj++) {
+                    criteria = outputSummary[item].criteria[jj];
+                    if (!(typeof criteria.minpop == 'undefined' || !criteria.minpop || total[criteria.unit].population >= criteria.minpop)) isValid = false;
+                    if (!(typeof criteria.maxpop == 'undefined' || !criteria.maxpop || total[criteria.unit].population < criteria.maxpop)) isValid = false;
+                }
+                if (isValid) {
+                    summary[item].coords.push(village.coords);
+                    summary[item].tally++;
+                }
+            }
+        }
+    }
+
+    var groupSummary = {};
+    for (item in outputSummary) {
+        if (outputSummary.hasOwnProperty(item)) {
+            if (typeof groupSummary[outputSummary[item].group] == 'undefined') groupSummary[outputSummary[item].group] = [];
+            groupSummary[outputSummary[item].group].push(item);
+        }
+    }
+
+    var curGroup = maxGroups;
+    totalTroops = summary.unitTotal.population;
+    const intPlayerPoints = parseInt(playerPoints);
+    let troopsToPointsRatio = 0.0;
+
+    if (intPlayerPoints !== 0) troopsToPointsRatio = (totalTroops / intPlayerPoints).toFixed(2);
+
+    setTimeout(function () { jQuery('#troopsPointsRatio').text(troopsToPointsRatio); }, 100);
+
+    var docSource = '';
+    docSource += '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">\n';
+    docSource += '<html><head><script type="text/javascript">';
+    docSource += 'function fnShowCoords(id,description){ var coords={};';
+    for (item in outputSummary) {
+        if (outputSummary.hasOwnProperty(item)) {
+            if (summary[item].coords.length) docSource += 'coords["' + item + '"] = "' + summary[item].coords.join(' ') + '";\n';
+        }
+    }
+    docSource += 'document.getElementById("coords_group").innerHTML = description;';
+    docSource += 'var eleCoords = document.getElementById("coords_container");';
+    docSource += 'eleCoords.value = coords[id]?coords[id]:""; eleCoords.focus(); eleCoords.select();}</script></head><body>';
+    docSource += '<table class="main" width="100%" align="center"><tr><td><h2>' + fnTranslate(curGroup++) + '<sup><span style="font-size:small;"></span></sup></h2>';
+    docSource += `${showPlayer}${showTroopsPointRatio}${currentGroup}${serverDateTime}`;
+    docSource += '<table class="not-draggable"><tr><td width="450" valign="top"><table class="vis" width="100%">';
+    
+    for (item in groupSummary) {
+        if (groupSummary.hasOwnProperty(item)) {
+            count = 0;
+            docSource += '<tr><th colspan="2">' + fnTranslate(curGroup++) + '</th></tr>';
+            for (jj = 0; jj < groupSummary[item].length; jj++) {
+                docSource += '<tr class="' + (count++ % 2 ? 'row_b' : 'row_a') + '">';
+                docSource += '<td width="240" style="white-space:nowrap;"><a href="#" onclick="fnShowCoords(\'' + groupSummary[item][jj] + "','" + fnTranslate(outputSummary[groupSummary[item][jj]].descID) + '\');">&raquo;&nbsp; ' + fnTranslate(outputSummary[groupSummary[item][jj]].descID) + '</a></td>\n';
+                docSource += '<td width="240"' + (summary[groupSummary[item][jj]].tally > 0 ? '' : ' class="hidden"') + ' style="text-align:right;"><span>' + summary[groupSummary[item][jj]].tally + '</span></td></tr>';
+            }
+        }
+    }
+    docSource += '</table><td valign="top"><table class="vis" width="100%"><tr><th colspan="2" style="white-space:nowrap;">' + fnTranslate(curGroup++) + '</th></tr>';
+    count = 0;
+    for (key in offense) {
+        if (offense.hasOwnProperty(key)) {
+            docSource += '<tr class="' + (count++ % 2 ? 'row_b' : 'row_a') + '"><td><img src="https://' + location.hostname + '/graphic/unit/unit_' + offense[key] + '.png?1" alt=""/></td><td style="white-space:nowrap;"><span> ' + formatAsNumber(summary[offense[key]].count) + ' ' + unitDesc[offense[key]] + '</span></td></tr>';
+        }
+    }
+    docSource += '</table><table class="vis" width="100%"><tr><th colspan="2" style="white-space:nowrap;">' + fnTranslate(curGroup++) + '</th></tr>';
+    count = 0;
+    for (key in defense) {
+        if (defense.hasOwnProperty(key)) {
+            docSource += '<tr class="' + (count++ % 2 ? 'row_b' : 'row_a') + '"><td><img src="https://' + location.hostname + '/graphic/unit/unit_' + defense[key] + '.png?1" alt=""/></td><td style="white-space:nowrap;"><span> ' + formatAsNumber(summary[defense[key]].count) + ' ' + unitDesc[defense[key]] + '</span></td></tr>';
+        }
+    }
+    docSource += '</table><table class="vis" width="100%"><tr><th colspan="2" style="white-space:nowrap;">' + fnTranslate(curGroup++) + '</th></tr>';
+    count = 0;
+    $(unitConfig).children().each(function (i, e) {
+        var unit = e.nodeName;
+        if (!new RegExp('^(' + defense.join('|') + '|' + offense.join('|') + ')$').test(unit)) {
+            docSource += '<tr class="' + (count++ % 2 ? 'row_b' : 'row_a') + '"><td><img src="https://' + location.hostname + '/graphic/unit/unit_' + unit + '.png?1" alt=""/></td><td style="white-space:nowrap;"><span> ' + formatAsNumber(summary[unit].count) + ' ' + unitDesc[unit] + '</span></td></tr>';
+        }
+    });
+    docSource += '</table><table class="vis" width="100%"><tr><th colspan="2" style="white-space:nowrap;">' + fnTranslate(curGroup++) + '</th></tr>';
+    docSource += '<tr class="row_a"><td><span>Count:</span></td><td style="white-space:nowrap;"><span> ' + formatAsNumber(summary.unitTotal.tally) + '</span></td></tr>';
+    docSource += '<tr class="row_b"><td><span>Pop:</span></td><td style="white-space:nowrap;"><span> ' + formatAsNumber(summary.unitTotal.population) + '</span></td></tr>';
+    docSource += '</table></td></td></tr></table><br>';
+    
+    docSource += '<table id="coordinate_table" class="vis" style="width:100%;">';
+    docSource += '<tr><th>' + fnTranslate(curGroup++) + ': <span id="coords_group" style="font-weight:100;"></span>';
+    docSource += '<tr><td style="box-sizing:border-box;width:100%;"><textarea id="coords_container" style="resize:none;width:100%;box-sizing:border-box;height:60px;"></textarea></td></tr>';
+    docSource += '</table>';
+    
+    docSource += '<div style="padding: 10px 0;">';
+    docSource += '<button id="btnSendDiscord" class="btn btn-default" style="width: 100%; background-color: #5865F2; color: white; padding: 10px; border-radius: 4px; font-weight: bold; border: none; cursor: pointer; transition: 0.2s;">📤 Enviar para o Discord</button>';
+    docSource += '</div>';
+
+    docSource += `<small><strong>Troops Counter ${strVersion}</strong> - Modified for Discord</small></body></html>`;
+
+    if (DRAGGABLE) {
+        docSource += `<a class="popup_box_close custom-close-button" onClick="clickDraggableEl();" href="#">&nbsp;</a>
+            <style>.troops-counter-content { position: fixed; background-color: #f4e4bc !important; border: 1px solid #603000; padding: 10px; top: 10vh; right: 15vw; z-index: 1000; width: 440px; height: auto; } .custom-close-button { right: 0; top: 0; } #btnSendDiscord:hover { background-color: #4752C4 !important; }</style>`;
+        if (jQuery('.troops-counter-content').length > 0) {
+            UI.ErrorMessage('Troops Counter is already loaded!');
+        } else {
+            var troopsCounterContent = document.createElement('div');
+            troopsCounterContent.classList.add('troops-counter-content');
+            troopsCounterContent.innerHTML = docSource;
+            jQuery('body').append(troopsCounterContent);
+            jQuery('.troops-counter-content').draggable({ cancel: '.not-draggable' });
+            jQuery('#btnSendDiscord').on('click', fnSendToDiscord);
+        }
+    } else {
+        const popupContent = preparePopupContent(docSource, '440px');
+        Dialog.show('content', popupContent);
+        jQuery('#btnSendDiscord').on('click', fnSendToDiscord);
+        jQuery('head').append('<style>#btnSendDiscord:hover { background-color: #4752C4 !important; }</style>');
+    }
+}
+
+function clickDraggableEl() { jQuery('.troops-counter-content').remove(); }
+
+function initDebug() { console.debug(`[Troops Counter] Script Loaded`); }
+
+function formatAsNumber(number) { return parseInt(number).toLocaleString('pt-PT'); }
+
+// -------------------------------------------------------------------------
+// FUNÇÃO DE VERIFICAÇÃO CORRIGIDA AQUI
+// -------------------------------------------------------------------------
+function checkScreen(userScreen, userMode) {
+    var isScreen = (game_data.screen === userScreen) || window.location.href.includes('screen=' + userScreen);
+    var isMode = (game_data.mode === userMode) || window.location.href.includes('mode=' + userMode);
+    return isScreen && isMode;
+}
+
+function preparePopupContent(popupBody, maxWidth) { return `<div class="ra-body" style="max-width: ${maxWidth}">${popupBody}</div>`; }
+
+(async function () {
+    if (!game_data.features.Premium.active) {
+        UI.ErrorMessage('A Conta Premium precisa de estar ativa para este script funcionar!', 6000);
+    } else {
+        fnExecuteScript();
+    }
+})();
